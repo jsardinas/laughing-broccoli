a1772d4c3b51a433e42bccdec5deae7d817d78c2
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-20 13:17:27 -0500

    Setting up models for User and Ad

---
+++ client/src/components/models/Ad.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    email: {
+        type: String,
+        required: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Must use a valid email address'],
+
+    }
+    ads_id: {
+        type: [],
+    }
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
