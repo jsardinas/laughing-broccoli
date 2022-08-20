6db49fdb5b1aae5b45c67789cb0bed1c1ec59e54
Javier <javier.sardinas@gmail.com>
2022-08-20 12:57:49 -0500

    Add connection string

---
+++ server/config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ads-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
