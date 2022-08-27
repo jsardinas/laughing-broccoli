ce326f7f12c9eb4b90e37ca482fc1303b75bab7e
Connor Blake <cblake506@gmail.com>
2022-08-27 13:07:51 -0500

    undid revert on helpers.js

---
+++ client/src/components/utils/helpers.js
@@ -0,0 +1,12 @@
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export function checkPassword(input) {
+    const passw = /^[A-Za-z]\w{7,14}$/;
+    if (input.match(passw)) {
+        return true;
+    }
+    return false;
+}
