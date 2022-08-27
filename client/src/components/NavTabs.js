0038911ff6142db3f9b6dbdbc7d3af3f22ff30be
Connor Blake <cblake506@gmail.com>
2022-08-27 12:12:26 -0500

    fixing navbar bugs

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -8,7 +8,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
             <ul className="nav nav-tabs">
                 <li className="nav-item">
                     <a
-                        href="logIn"
+                        href="#logIn"
                         onClick={() => handlePageChange('LogIn')}
                         // This is a conditional (ternary) operator that checks to see if the current page is "Home"
                         // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -19,7 +19,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                 </li>
                 <li className="nav-item">
                     <a
-                        href="classifieds"
+                        href="#classifieds"
                         onClick={() => handlePageChange('Classifieds')}
                         // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                         className={currentPage === 'Classifieds' ? 'nav-link active' : 'nav-link'}
@@ -29,7 +29,7 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                 </li>
                 <li className="nav-item">
                     <a
-                        href="userAd"
+                        href="#userAd"
                         onClick={() => handlePageChange('UserAd')}
                         // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                         className={currentPage === 'UserAd' ? 'nav-link active' : 'nav-link'}
