d2aada3c522ebd39f89eb058eb570f273aa8a235
Rocket Appliances <monaco.vin@gmail.com>
2022-08-28 15:46:15 -0500

    ADD create user, login user, jwt token in localstorage

--- client/src/components/pages/LogIn.js
+++ client/src/components/pages/LogIn.js
@@ -1,103 +1,137 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "../../utils/axios";
+import { useNavigate } from "react-router-dom";
 
 // Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from '../../utils/helpers';
+import { checkPassword, validateEmail } from "../../utils/helpers";
 
 function Form() {
-    // Create state variables for the fields in the form
-    // We are also setting their initial values to an empty string
-    const [email, setEmail] = useState('');
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [isLogInForm, setIsLogInForm] = useState(true);
+  // const navigate = useNavigate();
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLogInForm, setIsLogInForm] = useState(true);
+  const showLogInForm = (value) => {
+    setIsLogInForm(value);
+    console.log(value);
+  };
 
-    const showLogInForm = (value) => {
-        setIsLogInForm(value)
-        console.log(value);
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "userName") {
+      setUserName(inputValue);
+    } else {
+      setPassword(inputValue);
     }
+  };
 
-    const handleInputChange = (e) => {
-        // Getting the value and name of the input which triggered the change
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+  const handleFormSubmit = async (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
 
-        // Based on the input type, we set the state of either email, username, and password
-        if (inputType === 'email') {
-            setEmail(inputValue);
-        } else if (inputType === 'userName') {
-            setUserName(inputValue);
-        } else {
-            setPassword(inputValue);
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if ((!isLogInForm && !validateEmail(email)) || !userName) {
+      setErrorMessage("Email or username is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    if (!checkPassword(password)) {
+      setErrorMessage(
+        `Choose a more secure password for the account: ${userName}`
+      );
+      return;
+    }
+    // alert(`Hello ${userName}`);
+    const request = isLogInForm ? "/login" : "/register";
+    // try {
+    const res = await axios
+      .post(request, {
+        email,
+        username: userName,
+        password,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status == 200) {
+          if (request == "/login") {
+            localStorage.setItem("token", res.data.token);
+            window.history.pushState({}, "", "/abc");
+            // navigate("/abt");
+            // alert("user login successful");
+          }
+          if (request == "/register")
+            alert("user account created successfully");
         }
-    };
+      })
+      .catch((error) => {
+        alert(error.response.data.error);
+      });
+    // } catch (error) {
+    //   alert(error.response.data.error);
+    // }
 
-    const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        e.preventDefault();
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setUserName("");
+    setPassword("");
+    setEmail("");
+  };
 
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email) || !userName) {
-            setErrorMessage('Email or username is invalid');
-            // We want to exit out of this code block if something is wrong so that the user can correct it
-            return;
-            // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-        }
-        if (!checkPassword(password)) {
-            setErrorMessage(
-                `Choose a more secure password for the account: ${userName}`
-            );
-            return;
-        }
-        alert(`Hello ${userName}`);
+  return (
+    <div>
+      <p>Hello {userName}</p>
+      <button type="button" onClick={() => showLogInForm(true)}>
+        Log in
+      </button>
+      <button type="button" onClick={() => showLogInForm(false)}>
+        Create account
+      </button>
+      <form className="form">
+        {isLogInForm === true ? null : (
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
+        )}
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+        />
 
-        // If everything goes according to plan, we want to clear out the input after a successful registration.
-        setUserName('');
-        setPassword('');
-        setEmail('');
-    };
-
-    return (
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
         <div>
-            <p>Hello {userName}</p>
-            <button type="button" onClick={() => showLogInForm(true)}>Log in</button>
-            <button type="button" onClick={() => showLogInForm(false)} >Create account</button>
-            <form className="form">
-                {isLogInForm === true ?
-                    null :
-                    <input
-                        value={email}
-                        name="email"
-                        onChange={handleInputChange}
-                        type="email"
-                        placeholder="email"
-                    />}
-                <input
-                    value={userName}
-                    name="userNames"
-                    onChange={handleInputChange}
-                    type="text"
-                    placeholder="username"
-                />
-                <input
-                    value={password}
-                    name="password"
-                    onChange={handleInputChange}
-                    type="password"
-                    placeholder="Password"
-                />
-
-                <button type="button" onClick={handleFormSubmit}>Submit</button>
-            </form>
-            {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
-                </div>
-            )}
+          <p className="error-text">{errorMessage}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Form;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const { User } = require("./models");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(cors());
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,27 +19,56 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
+app.post("/register", async (req, res) => {
+  const users = await User.find({ email: req.body.email });
+  if (users.length != 0) {
+    return res.status(400).json({
+      error: "user email already exists",
+    });
+  }
+  const _user = new User(req.body);
+  _user.save((error, result) => {
+    if (error) {
+      return res.status(400).json(error);
+    }
+    return res.status(200).json({});
+  });
+});
+app.post("/login", async (req, res) => {
+  const users = await User.find({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  if (users.length == 0) {
+    return res.status(400).json({ error: "username or password incorrect" });
+  }
+  const token = jwt.sign({ username: req.body.username }, "vincent");
+  return res.status(200).json({
+    token,
+  });
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-  
-  db.once('open', () => {
+
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
-  
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
