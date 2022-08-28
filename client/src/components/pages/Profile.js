31f09e750ecd5ca670107fc424cd29784a6f0bf8
Sofia <asofiae96@gmail.com>
2022-08-28 14:13:31 -0500

    fixing profile link

---
+++ client/src/components/pages/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useQuery } from '@apollo/client';
+import { QUERY_AD } from '../utils/queries';
+import ClassifiedsAdCard from "./ClassifiedsAdCard";
+
+export default function Classifieds() {
+    const { loading, error, data } = useQuery(QUERY_AD);
+    console.log('loading:', loading);
+    console.log('error:', error);
+    console.log('data:', data);
+    const adInfo = data?.ads || [];
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    return (
+        <div>
+            {adInfo.map((ad) => (
+                <ClassifiedsAdCard title={ad.title} description={ad.description} username={ad.username} date={ad.date}/>
+            ))}
+
+        </div>
+    );
+}
