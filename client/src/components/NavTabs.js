709c0d3e6133ad9dd79416185e933c7c627e473d
Javier <javier.sardinas@gmail.com>
2022-08-30 13:29:51 -0500

    Add logout

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -1,17 +1,18 @@
 import React from 'react';
-
 import DateTime from './DateTime';
+import Auth from '../utils/auth';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 export default function NavTabs({ currentPage, handlePageChange }) {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+      };
     return (
         <>
             <ul className="nav nav-tabs">
 
-
-            
-                
                 <li className="nav-item">
                     <a
                         href="#classifieds"
@@ -22,9 +23,6 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                         Classifieds
                     </a>
                 </li>
-
-
-
 
                 <li className="nav-item">
                     <a
@@ -37,21 +35,28 @@ export default function NavTabs({ currentPage, handlePageChange }) {
                     </a>
                 </li>
 
-
-
                 <li className="nav-item">
-                    <a
-                        href="#logIn"
-                        onClick={() => handlePageChange('LogIn')}
-                        // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-                        // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-                        className={currentPage === 'LogIn' ? 'nav-link active' : 'nav-link'}
-                    >
-                        Login
-                    </a>
+                     {Auth.loggedIn() ? 
+                        (<a
+                            href="#logIn"
+                            onClick={logout}
+                            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+                            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+                            className={currentPage === 'LogIn' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Logout
+                        </a>) : (
+                        <a
+                            href="#logIn"
+                            onClick={() => handlePageChange('LogIn')}
+                            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+                            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+                            className={currentPage === 'LogIn' ? 'nav-link active' : 'nav-link'}
+                        >
+                            Login
+                        </a>)
+                    }
                 </li>
-
-
 
 <li>
 <DateTime></DateTime>
